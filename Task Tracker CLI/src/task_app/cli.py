b8a012fef6d93abd02b6755e0beb12fc8b6a57e5
Subham Das <subhamdas@Subhams-Mac-M2-air.local>
2026-05-10 14:16:02 +0530

    Done with the CLI

---
+++ Task Tracker CLI/src/task_app/cli.py
@@ -0,0 +1,47 @@
+
+from .database import Database_manager
+from .models import Task,TaskStatus
+from typer import Typer
+
+
+app = typer.Typer()
+db = Database_manager()
+
+@app.command
+def add(title: str,description:str = typer.Arguement(""),due_date:Optional[str]= None):
+
+    db.add_task(Task(id=None,title = title, description=description,due_date=due_date))
+
+
+@app.command
+def update_status(id:int,status:str):
+
+    valid_status = [s for s in TaskStatus]
+
+    if status not in valid_status:
+        typer.echo(f"Invalid status. Choose either of these {valid_status}")
+        raise typer.Exit()
+    
+    
+    db.update_status_task(title,status)
+
+@app.command
+def list_task(status: Optional[str]= None):
+    if status:
+        valid_status = [s.value for s in TaskStatus]
+        
+        if status not in valid_status:
+            typer.echo(f"Invalid status command. Try with these {status}")
+            raise typer.Exit()
+    
+    tasks = db.get_task_by_status(status)
+
+    if not tasks:
+        typer.echo('No task found.')
+        return
+    
+    for task in tasks:
+        typer.echo(f"{task[0]}. [{task[3]}] {task[1]}: {task[2]}")
+    
+if __name__ == '__main__':
+    app()
--- Task Tracker CLI/src/task_app/database.py
+++ Task Tracker CLI/src/task_app/database.py
@@ -1,28 +1,69 @@
 import sqlite3 
-from .models import TaskStatus
+from .models import TaskStatus,Task
 
 class Database_manager:
     def __init__(self,db_path = 'data.db'):
-        self.connection = sqlite3.connect('db_path')
-        self.cursor = self.connection.cursor
+        self.connection = sqlite3.connect(db_path)
+        self.cursor = self.connection.cursor()
         
     def create_table(self):
         self.cursor.execute(
             '''
                 CREATE TABLE task(
                 
-                    id INT PRIMARY KEY,
+                    id INT PRIMARY KEY AUTOINCREMENT,
                     title TEXT NOT NULL,
                     description TEXT,
                     status TEXT DEFAULT 'todo',
                     created_at TEXT,
                     due_date TEXT
-                )
+                );
             '''
         )
+        self.connection.commit()
+
+
+    def add_task(self,task):
+
+        query  =  '''
+                INSERT INTO task(title,description,status,created_at)
+                VALUES(?,?,?,?);
+        '''
+
+        values = (
+            task.title,
+            task.description,
+            task.created_at.isoformat(),
+            task.due_date.isoformat()
+        )
+
+        self.cursor.execute(query,values)
+        self.connection.commit()
+
+    def update_status_task(self,id,status):
+        
+        query = '''
+                UPDATE task
+                SET status = ?
+                WHERE id = ?
+            '''
+        
+        self.cursor.execute(query,status,id)
 
         self.connection.commit()
-    
+
+    def get_task_by_status(self, status:str= None):
+
+        if status:
+            query = "SELECT * FROM task WHERE status = ?"
+            self.cursor.execute(query,(status,))
+        
+        else:
+            query = "SELECT * FROM task"
+            self.cursor.execute(query)
+        
+        self.cursor.fetchall()
+
 
     def close(self):
         self.connection.close()
